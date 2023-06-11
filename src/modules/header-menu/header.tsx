5d24311c82044c95b49d7136f9c20b165a0c1b24
hyperlolorb@gmail.com <hyperlolorb@gmail.com>
2023-06-10 20:01:03 -0500

    style: changed header style to better work with the page

--- src/modules/header-menu/header.tsx
+++ src/modules/header-menu/header.tsx
@@ -6,12 +6,17 @@ import "./styles.css";
 export function Header() {
   return (
     <header className="header">
+      <nav>
         <Link to="/" className="logo">
           Express Auto Glass
         </Link>
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" htmlFor="menu-btn">
+        {/* <input className="menu-btn" type="checkbox" id="menu-btn" /> */}
+        {/* <label className="menu-icon" htmlFor="menu-btn">
           <span className="navicon"></span>
+        </label> */}
+        <input type="checkbox" id="menu-toggle" />
+        <label htmlFor="menu-toggle" className="menu-icon">
+          &#9776;
         </label>
         <ul className="menu">
           <li>
@@ -24,6 +29,7 @@ export function Header() {
             <Link to="/faq">FAQ</Link>
           </li>
         </ul>
+      </nav>
     </header>
   );
 }
