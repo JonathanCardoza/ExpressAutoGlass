cf059840034a3ed35985dabacfc7a26b0b713811
hyperlolorb@gmail.com <hyperlolorb@gmail.com>
2023-06-19 21:38:23 -0500

    feat: made accordion arrow function properly on click

--- src/modules/accordion/accordion.tsx
+++ src/modules/accordion/accordion.tsx
@@ -10,7 +10,6 @@ export function Accordion({
   accordionContent,
   label,
 }: AccordionProps): JSX.Element {
-    
   const [isOpen, toggleOpen] = useState(false);
 
   function handleToggle() {
@@ -18,15 +17,18 @@ export function Accordion({
     toggleOpen(!isOpen);
   }
 
+  const cnParts = ["accordion"];
+
+  if (isOpen) {
+    cnParts.push("accordion-open");
+  }
+
+  const cn = cnParts.join(" ");
+
   return (
-    <div className="accordion">
+    <div className={cn}>
       <div className="accordion-header"></div>
-      <button
-        type="button"
-        className="accordion-button"
-        onClick={handleToggle}
-        title="hello"
-      >
+      <button type="button" className="accordion-button" onClick={handleToggle}>
         {label}
       </button>
       {isOpen && <div className="accordion-item">{accordionContent}</div>}
